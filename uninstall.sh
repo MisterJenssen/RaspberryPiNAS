4ab64aed0046f85fc002444f0c58b7d57a70de00
MisterJenssen <richardjanssen8@hotmail.com>
2023-01-22 15:53:06 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,10 +2,7 @@
 # uninstall minitowerkit script 
 . /lib/lsb/init-functions
 
-moodlight_svc="minitower_moodlight.service"
-filelocation="/lib/systemd/system/${moodlight_svc}.service"
-
-oled_svc="minitower_oled.service"
+oled_svc="minitower_display_moodlight.service"
 svclocation="/lib/systemd/system/${oled_svc}"
 
 fan_pwm_svc="minitower_fan_pwm.service"
@@ -17,10 +14,6 @@ sleep 1
 log_action_msg "Remove dtoverlay configure from /boot/config.txt file"
 sudo sed -i '/dtparam=i2c.*/ s/^/#/' /boot/config.txt
 
-log_action_msg "Stop and disable ${moodlight_svc}"
-sudo systemctl disable ${moodlight_svc} 2&>/dev/null  
-sudo systemctl stop ${moodlight_svc}  2&>/dev/null
-
 log_action_msg "Stop and disable ${oled_svc}"
 sudo systemctl disable ${oled_svc} 2&>/dev/null  
 sudo systemctl stop ${oled_svc}  2&>/dev/null
@@ -30,8 +23,6 @@ sudo systemctl disable ${fan_pwm_svc} 2&>/dev/null
 sudo systemctl stop ${fan_pwm_svc}  2&>/dev/null
 
 log_action_msg "Remove Minitower kit Driver..."
-sudo rm -f  /lib/systemd/system/minitower_moodlight.service  2&>/dev/null 
-sudo rm -f /usr/bin/moodlight 2&>/dev/null 
 sudo rm -rf /lib/systemd/system/minitower_oled.service 2&>/dev/null
 sudo rm -rf /lib/systemd/system/minitower_fan_pwm.service 2&>/dev/null
 
