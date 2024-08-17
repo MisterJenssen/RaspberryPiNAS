80ae77e1c2d533030a7b6bcb977e7aff9944691d
MisterJenssen <richardjanssen8@hotmail.com>
2024-08-17 16:44:48 +0200

    Using led brightness now instead of lesser color & increased main loop delay

--- sysinfo.py
+++ sysinfo.py
@@ -25,14 +25,14 @@ LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 25   # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-COLOR_RED = Color(30, 0, 0)
-COLOR_ORANGE = Color(30, 10, 0)
-COLOR_GREEN = Color(0, 30, 0)
-COLOR_BLUE = Color(0, 0, 30)
+COLOR_RED = Color(255, 0, 0)
+COLOR_ORANGE = Color(255, 100, 0)
+COLOR_GREEN = Color(0, 255, 0)
+COLOR_BLUE = Color(0, 0, 255)
 COLOR_OFF = Color(0, 0, 0)
 
 
@@ -177,4 +177,4 @@ if __name__ == '__main__':
                     disk_usage('/mnt'))
         
         controlMoodLight(ip, temperature)
-        time.sleep(5)
+        time.sleep(30)
